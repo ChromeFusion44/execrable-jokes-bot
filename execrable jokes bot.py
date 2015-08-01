294468258130ea074ba06fd1bebef50a4dfd520c
ChromeFusion44 <Latios43@gmail.com>
2015-08-01 02:38:01 -0400

    It actually works now
    I basically changed everything and now it works! I want to add even more
    horrible jokes tbh

--- execrable jokes bot.py
+++ execrable jokes bot.py
@@ -1,41 +1,46 @@
 import random
 import time
+import sys
 
 def tellJoke():
-    #Let's tell some absolutely horrendous jokes
-    #jokes = {'jack': 4098, 'sape': 4139}
+    #Let's tell some absolutely horrendous jokes.
     
-    #List of Jokes 
-    joke0x0 = "Why was six afraid of seven?"
-    joke0x1 = "It wasn't. Numbers are not sentient and thus incapable of feeling fear."
+    #I could have stored all of the jokes and punchlines in a list myself,
+    #but that would be horribly messy and end up as one ludicrously long line 
+    #of code. So no. I'll just append them into the jokes and punchlines
+    #variables.
     
-    joke1x0 = "How do you make a plumber sad?"
-    joke1x1 = "You murder their family."
+
+    #List of Jokes
+    jokes = []
+    jokes.append("Why was six afraid of seven?")
+    jokes.append("How do you make a plumber sad?")
+    jokes.append("When SCUBA diving, why is it important to fall backward off the side of the boat?")
+    jokes.append("What do you call a shady man selling drugs?")
+    jokes.append("Where was the Declaration of Independence signed?")
+    jokes.append("A cat walks into a bar and orders a bowl of milk.")
+    jokes.append("Why are you reading these jokes?")
     
-    joke2x0 = "When SCUBA diving, why is it important to fall backward off the side of the boat?"
-    joke2x1 = "Because if you fell forward, you would still be in the boat."
-    
-    joke3x0 = "What do you call a shady guy selling drugs?"
-    joke3x1 = "A pharmacist."
-    
-    joke4x0 = "Where was the Declaration of Independence signed?"
-    joke4x1 = "The bottom of the paper."
-    
-    joke5x0 = "A cat walks into a bar and orders a bowl of milk."
-    joke5x1 = "The bartender, realizing that cats cannot talk nor do they posses higher brain functions, realizes he must be dreaming."
-    
-    joke6x0 = "Why are you reading these jokes?"
-    joke6x1 = "Because you have no life."
-    
-    joke7x0 = ""
-    joke7x1 = ""
-    
+    #List of Punchlines
+    punchlines = []
+    punchlines.append("It wasn't. Numbers are not sentient and thus incapable of feeling fear.")
+    punchlines.append("You murder their family.")
+    punchlines.append("Because if you fell forward, you would still be in the boat.")
+    punchlines.append("A pharmacist.")
+    punchlines.append("The bottom of the paper.")
+    punchlines.append("The bartender, realizing that cats cannot talk nor do they posses higher brain functions, realizes he is hallucinating.")
+    punchlines.append("Because you have no life.")
     
     #Joke Telling System
-    jokeNum = str(random.randint(0, 6))    
-    jokeToTell1 = "joke"+jokeNum+"x1"
-    jokeToTell2 = "joke"+jokeNum+"x2"
-    
-    print jokeToTell1
-    time.sleep(2)
-    print jokeToTell2
+    #
+    #Generate a pseudorandom number between 0 and 6
+    #Use that number to print a random joke and
+    #its corresponding punchline.
+    #
+    #The sys command is required because python
+    #doesn't pause correctly without it.
+    jokeNumber = random.randint(0,6)
+    print jokes[jokeNumber]
+    sys.stdout.flush()
+    time.sleep(3)
+    print punchlines[jokeNumber]
